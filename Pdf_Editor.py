887c689186fc45557d4939cbba40b4d5d600537b
Akash_Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-11-24 19:52:04 +0530

    Add files via upload

---
+++ Pdf_Editor.py
@@ -0,0 +1,13 @@
+#Pip install pikepdf
+import pikepdf
+
+old_pdf = pikepdf.pdf.open("C:\Python_AK\Akash_CV.pdf")
+
+#old_pdf.pages[4] = old_pdf.pages[0]
+#old_pdf.save("Rep_New.pdf")
+
+#del old_pdf.page[1:3]
+#old_pdf.save("Del_New.pdf")
+
+#del old_pdf.pages.reverse()
+#old_pdf.save(Rev.New.pdf")
